5bee3a6408b3d30a1f04a715db5e2a037851640a
Nerwyn Singh <nerwyn1994@gmail.com>
2023-12-04 20:16:23 -0500

    repo setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'production',
+	entry: {
+		main: './src/service-call-tile-feature.ts',
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'service-call-tile-feature.js',
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader',
+			},
+		],
+	},
+};
