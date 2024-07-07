bd9630017136212e13e096c1e52e7c99d5b6e6e5
Nerwyn Singh <nerwyn1994@gmail.com>
2024-07-07 04:07:00 -0400

    move renderTemplate definition to index

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,9 @@
-export { renderTemplate } from './renderTemplate';
+import { HomeAssistant } from 'custom-card-helpers';
+/**
+ * Render a Home Assistant template string using nunjucks
+ * @param {HomeAssistant} hass The Home Assistant object
+ * @param {string} str The template string to render
+ * @param {object} [context] Additional context to expose to nunjucks
+ * @returns {string | boolean} The rendered template string if a string was provided, otherwise the unaltered input
+ */
+export declare function renderTemplate(hass: HomeAssistant, str: string, context?: object): string | boolean;
