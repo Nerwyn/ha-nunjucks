eff8dfe3327db01403a1cf71b872b522b7c081a2
Nerwyn Singh <nerwyn1994@gmail.com>
2024-09-11 23:50:09 -0400

    update py-datetime to version with proper index.js

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,7 +4,6 @@ module.exports = {
 	testEnvironment: 'node',
 	testPathIgnorePatterns: ['/node_modules'],
 	moduleNameMapper: {
-		'py-datetime': '<rootDir>/node_modules/py-datetime/dist/index.min.js',
 		'd3-time-format':
 			'<rootDir>/node_modules/d3-time-format/dist/d3-time-format.min.js',
 		'd3-time': '<rootDir>/node_modules/d3-time/dist/d3-time.min.js',
