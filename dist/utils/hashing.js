488336403da74858b7ec446bc5390d4de4f6d4be
Nerwyn Singh <nerwyn1994@gmail.com>
2026-06-30 01:36:33 -0400

    switch hashing to @noble/hashes

--- dist/utils/hashing.js
+++ dist/utils/hashing.js
@@ -1,16 +1,23 @@
-import MD5 from 'jscrypto/MD5';
-import SHA1 from 'jscrypto/SHA1';
-import SHA256 from 'jscrypto/SHA256';
-import SHA512 from 'jscrypto/SHA512';
+import { md5 as md5_0, sha1 as sha1_0 } from '@noble/hashes/legacy.js';
+import { sha256 as sha256_0, sha512 as sha512_0 } from '@noble/hashes/sha2.js';
+function hash(value, algorithm) {
+    const msgUint8 = new TextEncoder().encode(value);
+    const hashBuffer = algorithm(msgUint8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+        .map((b) => b.toString(16).padStart(2, '0'))
+        .join('');
+    return hashHex;
+}
 export function md5(value) {
-    return MD5.MD5.hash(value).toString();
+    return hash(value, md5_0);
 }
 export function sha1(value) {
-    return SHA1.SHA1.hash(value).toString();
+    return hash(value, sha1_0);
 }
 export function sha256(value) {
-    return SHA256.SHA256.hash(value).toString();
+    return hash(value, sha256_0);
 }
 export function sha512(value) {
-    return SHA512.SHA512.hash(value).toString();
+    return hash(value, sha512_0);
 }
