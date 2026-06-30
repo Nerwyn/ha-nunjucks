45e42fe8121983cea64189ecff79540aecca4a3b
Nerwyn Singh <nerwyn1994@gmail.com>
2026-06-30 01:45:39 -0400

    switch hashing to hasha

--- src/utils/hashing.ts
+++ src/utils/hashing.ts
@@ -1,28 +1,21 @@
-import { md5 as md5_0, sha1 as sha1_0 } from '@noble/hashes/legacy.js';
-import { sha256 as sha256_0, sha512 as sha512_0 } from '@noble/hashes/sha2.js';
-
-function hash(value: string, algorithm: CallableFunction) {
-	const msgUint8 = new TextEncoder().encode(value);
-	const hashBuffer = algorithm(msgUint8);
-	const hashArray = Array.from(new Uint8Array(hashBuffer));
-	const hashHex = hashArray
-		.map((b) => b.toString(16).padStart(2, '0'))
-		.join('');
-	return hashHex;
-}
+import { hashSync } from 'hasha';
 
 export function md5(value: string) {
-	return hash(value, md5_0);
+	// return hash(value, md5_0);
+	return hashSync(value, { algorithm: 'md5' });
 }
 
 export function sha1(value: string) {
-	return hash(value, sha1_0);
+	// return hash(value, sha1_0);
+	return hashSync(value, { algorithm: 'sha1' });
 }
 
 export function sha256(value: string) {
-	return hash(value, sha256_0);
+	// return hash(value, sha256_0);
+	return hashSync(value, { algorithm: 'sha256' });
 }
 
 export function sha512(value: string) {
-	return hash(value, sha512_0);
+	// return hash(value, sha512_0);
+	return hashSync(value, { algorithm: 'sha512' });
 }
